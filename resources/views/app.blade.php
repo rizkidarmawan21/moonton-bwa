73720289f245f5c4f981dc6986089d7f35f82c0a
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2022-11-05 18:51:54 +0700

    feat: register

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Moonton') }}</title>
 
     <style>
         @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap");
